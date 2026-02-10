37da6625367acab780700ed305177f2e9ad3a1fd
KRISH KATIYAR <krishkatiyar58@gmail.com>
2026-02-10 23:28:09 +0530

    Add files via upload

---
+++ First.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int num=2;
+    if(num%2== 0){
+        cout<<"num is even";
+    }
+    else {
+        cout<<"num is odd" ;
+    }
+
+    return 0;
+}
